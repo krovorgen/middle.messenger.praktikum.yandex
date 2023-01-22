ebb565f94748db56c6b25ad512e67859f3355e07
maksim <krovorgen@gmail.com>
2023-01-22 11:06:16 +0300

    eslint config + Block.ts + EventBus.ts

---
+++ src/utils/Block.ts
@@ -0,0 +1,163 @@
+import EventBus from './EventBus';
+
+export default class Block {
+  static EVENTS = {
+    INIT: 'init',
+    FLOW_CDM: 'flow:component-did-mount',
+    FLOW_CDU: 'flow:component-did-update',
+    FLOW_RENDER: 'flow:render',
+  };
+
+  _element = null;
+
+  _meta = null;
+
+  constructor(tagName = 'div', props = {}) {
+    const eventBus = new EventBus();
+    this._meta = {
+      tagName,
+      props,
+    };
+
+    this.props = this._makePropsProxy(props);
+
+    this.eventBus = () => eventBus;
+
+    this._registerEvents(eventBus);
+    eventBus.emit(Block.EVENTS.INIT);
+  }
+
+  _registerEvents(eventBus) {
+    eventBus.on(Block.EVENTS.INIT, this.init.bind(this));
+    eventBus.on(Block.EVENTS.FLOW_CDM, this._componentDidMount.bind(this));
+    eventBus.on(Block.EVENTS.FLOW_CDU, this._componentDidUpdate.bind(this));
+    eventBus.on(Block.EVENTS.FLOW_RENDER, this._render.bind(this));
+  }
+
+  _createResources() {
+    const { tagName } = this._meta;
+    this._element = this._createDocumentElement(tagName);
+    this.eventBus().emit(Block.EVENTS.FLOW_CDM);
+  }
+
+  init() {
+    this._createResources();
+  }
+
+  _componentDidMount() {
+    this.componentDidMount();
+    this.eventBus().emit(Block.EVENTS.FLOW_RENDER);
+  }
+
+  // Может переопределять пользователь, необязательно трогать
+  componentDidMount(oldProps) {}
+
+  dispatchComponentDidMount() {
+    this.eventBus().emit(Block.EVENTS.FLOW_CDM);
+  }
+
+  _componentDidUpdate(oldProps, newProps) {
+    const response = this.componentDidUpdate(oldProps, newProps);
+    if (!response) {
+      return;
+    }
+    this.eventBus().emit(Block.EVENTS.FLOW_RENDER);
+  }
+
+  componentDidUpdate(oldProps, newProps) {
+    return true;
+  }
+
+  setProps = (nextProps) => {
+    if (!nextProps) {
+      return;
+    }
+
+    Object.assign(this.props, nextProps);
+  };
+
+  get element(): HTMLElement {
+    return this._element;
+  }
+
+  _render() {
+    const block = this.render();
+    this._removeEvents();
+    this._element.innerHTML = block;
+    this._addEvents();
+  }
+
+  // Может переопределять пользователь, необязательно трогать
+  render() {
+    return this.props.text;
+  }
+
+  getContent(): HTMLElement {
+    return this.element;
+  }
+
+  _makePropsProxy(props) {
+    // Можно и так передать this
+    // Такой способ больше не применяется с приходом ES6+
+
+    const self = this;
+    return new Proxy(props, {
+      get(target, prop) {
+        if (prop.indexOf('_') === 0) {
+          throw new Error('Отказано в доступе');
+        }
+
+        const value = target[prop];
+        return typeof value === 'function' ? value.bind(target) : value;
+      },
+      set(target, prop, value, receiver) {
+        target[prop] = value;
+        self.eventBus().emit(Block.EVENTS.FLOW_CDU, { ...target }, target);
+        return true;
+      },
+      deleteProperty() {
+        throw new Error('Нет доступа');
+      },
+    });
+  }
+
+  _createDocumentElement(tagName) {
+    // Можно сделать метод, который через фрагменты в цикле создаёт сразу несколько блоков
+    const element = document.createElement(tagName);
+    element.textContent = this.props.text;
+
+    return element;
+  }
+
+  _removeEvents() {
+    const { events } = this.props as any;
+
+    if (!events || !this._element) {
+      return;
+    }
+
+    Object.entries(events).forEach(([event, listener]) => {
+      this._element!.removeEventListener(event, listener);
+    });
+  }
+
+  _addEvents() {
+    const { events } = this.props as any;
+
+    if (!events) {
+      return;
+    }
+
+    Object.entries(events).forEach(([event, listener]) => {
+      this._element!.addEventListener(event, listener);
+    });
+  }
+
+  show() {
+    this.getContent().style.display = 'block';
+  }
+
+  hide() {
+    this.getContent().style.display = 'none';
+  }
+}
---
+++ src/utils/EventBus.ts
@@ -0,0 +1,38 @@
+type Handler = (...args: unknown[]) => void;
+
+class EventBus {
+  private readonly listeners: Record<string, Handler[]>;
+
+  constructor() {
+    this.listeners = {};
+  }
+
+  on(event: string, callback: Handler): void {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+
+    this.listeners[event].push(callback);
+  }
+
+  off(event: string, callback: Handler): void {
+    if (!this.listeners[event]) {
+      throw new Error(`Нет события: ${event}`);
+    }
+
+    this.listeners[event] = this.listeners[event].filter(
+      (listener) => listener !== callback,
+    );
+  }
+
+  emit(event: string, ...args: unknown[]): void {
+    if (!this.listeners[event]) {
+      throw new Error(`Нет события: ${event}`);
+    }
+
+    this.listeners[event].forEach((listener) => {
+      listener(...args);
+    });
+  }
+}
+export default EventBus;
