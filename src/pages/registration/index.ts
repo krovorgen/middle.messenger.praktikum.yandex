60e2ef9ec69c31b77be042423a7d677b6698f4a6
Maksim <krovorgen@gmail.com>
2023-01-31 18:41:37 +0300

    registrationPage

--- src/pages/registration/index.ts
+++ src/pages/registration/index.ts
@@ -1,58 +1,121 @@
 import tpl from './index.hbs';
-import link from '../../components/link';
-import formControl from '../../components/form-control';
-import button from '../../components/button';
+import { Block } from '../../utils/Block';
+import { renderDom } from '../../utils/renderDom';
+import { Button } from '../../components/button';
+import { Link } from '../../components/link';
+import { FormControl } from '../../components/form-control';
 
-document.getElementById('root')!.innerHTML = tpl({
-  emailField: formControl(
-    'email',
-    'Почта',
-    null,
-    'email',
-    'auth-box__label',
-  ),
-  phoneField: formControl(
-    'phone',
-    'Телефон',
-    null,
-    'tel',
-    'auth-box__label',
-  ),
-  firstNameField: formControl(
-    'first_name',
-    'Имя',
-    null,
-    'text',
-    'auth-box__label',
-  ),
-  secondNameField: formControl(
-    'second_name',
-    'Фамилия',
-    null,
-    'text',
-    'auth-box__label',
-  ),
-  loginField: formControl(
-    'login',
-    'Логин',
-    null,
-    'text',
-    'auth-box__label',
-  ),
-  passwordField: formControl(
-    'password',
-    'Пароль',
-    null,
-    'password',
-    'auth-box__label',
-  ),
-  repeatPasswordField: formControl(
-    'repeat_password',
-    'Пароль (ещё раз)',
-    null,
-    'password',
-    'auth-box__label',
-  ),
-  button: button('Зарегистрироваться', 'submit', 'auth-box__submit'),
-  link: link('../index.html', 'Назад к чатам', 'auth-box__link'),
+interface RegistrationPageProps {
+  button: Block
+  link: Block
+  addClass?: string
+  attr?: Record<string, string>
+  emailField: Block
+  phoneField: Block
+  firstNameField: Block
+  secondNameField: Block
+  loginField: Block
+  passwordField: Block
+  repeatPasswordField: Block
+}
+
+class RegistrationPage extends Block<RegistrationPageProps> {
+  constructor(props: RegistrationPageProps) {
+    super('div', {
+      ...props,
+      attr: {
+        class: `auth-box ${props.addClass ?? ''}`,
+        ...props.attr,
+      },
+    });
+  }
+
+  render() {
+    return this.compile(tpl, this.props);
+  }
+}
+
+const button = new Button({
+  text: 'Зарегистрироваться',
+  addClass: 'auth-box__submit',
+  size: 'sm',
+  variant: 'primary',
+  attr: {
+    type: 'submit',
+  },
+});
+
+const link = new Link({
+  text: 'Назад к чатам',
+  addClass: 'auth-box__link',
+  size: 'sm',
+  variant: 'primary',
+  attr: {
+    href: '../index.html',
+  },
+});
+
+const emailField = new FormControl({
+  type: 'email',
+  name: 'email',
+  placeholder: 'Почта',
+  addClass: 'auth-box__label',
+});
+
+const phoneField = new FormControl({
+  type: 'tel',
+  name: 'phone',
+  placeholder: 'Телефон',
+  addClass: 'auth-box__label',
+});
+
+const firstNameField = new FormControl({
+  type: 'text',
+  name: 'first_name',
+  placeholder: 'Имя',
+  addClass: 'auth-box__label',
+});
+
+const secondNameField = new FormControl({
+  type: 'text',
+  name: 'second_name',
+  placeholder: 'Фамилия',
+  addClass: 'auth-box__label',
+});
+
+const loginField = new FormControl({
+  type: 'text',
+  name: 'login',
+  placeholder: 'Логин',
+  addClass: 'auth-box__label',
+});
+
+const passwordField = new FormControl({
+  type: 'password',
+  name: 'password',
+  placeholder: 'Пароль',
+  addClass: 'auth-box__label',
+});
+
+const repeatPasswordField = new FormControl({
+  type: 'password',
+  name: 'repeat_password',
+  placeholder: 'Пароль (ещё раз)',
+  addClass: 'auth-box__label',
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  const homePage = new RegistrationPage({
+    button,
+    link,
+    emailField,
+    phoneField,
+    firstNameField,
+    secondNameField,
+    loginField,
+    passwordField,
+    repeatPasswordField,
+  });
+
+  renderDom('#app', homePage);
 });
