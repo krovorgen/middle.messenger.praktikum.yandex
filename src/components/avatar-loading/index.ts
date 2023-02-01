f4b72fd2eac66d8a938d0917e64ad0a844f2bff1
Maksim <krovorgen@gmail.com>
2023-02-01 04:08:25 +0300

    button to loading

--- src/components/avatar-loading/index.ts
+++ src/components/avatar-loading/index.ts
@@ -1,17 +1,20 @@
 import tpl from './avatar-loading.hbs';
 import { Block } from '../../core/Block';
+import { Button } from '../button';
 
 export default (button: any) => tpl({ button });
 
 interface AvatarLoadingProps {
   addClass?: string
   attr?: Record<string, string>
+  button?: Block
 }
 
 export class AvatarLoading extends Block<AvatarLoadingProps> {
   constructor(props: AvatarLoadingProps) {
     super('div', {
       ...props,
+      button: new Button({ size: 'sm', variant: 'primary', text: 'Сохранить' }),
       attr: {
         class: `avatar-loading ${props.addClass ?? ''}`,
         ...props.attr,
